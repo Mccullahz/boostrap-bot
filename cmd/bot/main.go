1c526055a046e94194dda64001a2c042a7061304
Mccullahz <zylardmccullah@gmail.com>
2026-02-09 13:42:26 -0500

    adjust compose setup, rework connection strings for cross platform compatability

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -4,6 +4,8 @@ package main
 import (
 	"log"
 	"fmt"
+	"os"
+	"strconv"
 
 	RLBot "github.com/Trey2k/RLBotGo"
 )
@@ -23,7 +25,7 @@ func getInput(gameState *RLBot.GameState, rlBot *RLBot.RLBot) *RLBot.ControllerS
 	}
 
 	if wasjustTouched && totalTouches <= 10 {
-		// DebugMessage is a helper function to let you quickly get debug text on screen. it will autmaicly place it so text will not overlap
+		// DebugMessage is a helper function to let you quickly get debug text on screen. it will automatically place it so text will not overlap
 		rlBot.DebugMessageAdd(fmt.Sprintf("The ball was touched %d times", totalTouches))
 		PlayerInput.Jump = false
 	} else if wasjustTouched && totalTouches > 10 {
@@ -37,10 +39,30 @@ func getInput(gameState *RLBot.GameState, rlBot *RLBot.RLBot) *RLBot.ControllerS
 func main() {
 	log.Println("Initializing boostrap-bot..")
 
-	// connect to RLBot
-	rlBot, err := RLBot.Connect(23234)
+	// connect to RLBot -- cross platform this doesnt work, so we need to env var the connection
+	host := os.Getenv("RLBT_HOST")
+	portStr := os.Getenv("RLBT_PORT")
+
+	// defaults if env vars are not set
+	if host == "" {
+		host = "127.0.0.1"
+		log.Println("RLBT_HOST not set, defaulting to localhost")
+	}
+	if portStr == "" {
+		portStr = "23234"
+		log.Println("RLBT_PORT not set, defaulting to 23234")
+	}
+	
+	port, err := strconv.Atoi(portStr)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+
+	// this should use host and port, only using port for now, this is likely to break -- circle back to this
+	rlBot, err := RLBot.Connect(port)
+	if err != nil {
+		log.Println("Failed to connect to RLBot, make sure the RLBot framework is running and the host and port are correct")
+		log.Fatal(err)
 	}
 
 	// Send ready message
